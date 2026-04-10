5aad4b9ca67aa8cbb9d3ff55c404f0d58bf6b5c4
provasetrr-gif <seu.email@exemplo.com>
2026-04-10 19:44:54 -0300

    commit ex33

---
+++ aula07-ex33.py
@@ -0,0 +1,26 @@
+'''
+calcula o resto da divisao
+desenvolva um codigo que leia 10 medias de aluno e no momento 
+que esta lendo mostre se o aluno esta aprovado se for maior que 5 
+ou em recuperacao no caso contrario;
+'''
+print(f"\nCalcula se aprovado ou nao  -Versao prog.: 1.1")
+print("="*80)
+media = 0
+#---------------------------------------------
+for i in range(1,11):
+    media = input(f"\n Digite a MEDIA {i} ==> ")
+    if  media == "" :
+        print("\n ERRO: Valor invalido  ")
+        exit()
+    media = float(media)
+    if media > 5:
+        tipo = "APROVADO"
+    else:
+        tipo = "EM RECUPERAÇÃO"
+    print(f"\tCom Media = {media} , Aluno esta {tipo}")
+#-------------------------------------------
+
+print("="*80)
+x = (input("\n Tecle Enter para finalizar ==> ")) 
+print(f"\nFinal do programa ")
